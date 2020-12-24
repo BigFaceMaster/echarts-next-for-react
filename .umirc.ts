0fc201721e5c0f83f33974410b285b88d077aee8
BigFaceMaster <BigFaceMaster@gmail.com>
2020-12-24 15:09:20 +0800

    Update config

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'dumi';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  title: 'echarts-next-for-react',
+  title: 'Echarts next for React',
   favicon: 'https://echarts.apache.org/zh/images/favicon.png?_v_=20200710_1',
   logo:
     'https://raw.githubusercontent.com/BigFaceMaster/echarts-next-for-react/master/docs/assets/logo.png',
@@ -11,6 +13,11 @@ export default defineConfig({
     ['en-US', 'English'],
     ['zh-CN', '中文'],
   ],
-  base: './echarts-next-for-react',
-  publicPath: "./"
+  base: '/echarts-next-for-react',
+  publicPath: './',
+  analytics: isProduction
+    ? {
+        ga: 'UA-154123333-1',
+      }
+    : false,
 });
